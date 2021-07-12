c57bb351700f2d1a9a816cce0086182396c86861
Macesuted <macesuted@qq.com>
2021-07-13 07:16:10 +0800

    update

---
+++ AtCoder/agc019_f.cpp
@@ -0,0 +1,88 @@
+/**
+ * @author Macesuted (macesuted@qq.com)
+ * @copyright Copyright (c) 2021
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace io {
+#define SIZE (1 << 20)
+char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
+int f, qr;
+inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
+inline char getch(void) { return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++); }
+inline void putch(char x) {
+    *oS++ = x;
+    if (oS == oT) flush();
+    return;
+}
+string getstr(void) {
+    string s = "";
+    char c = getch();
+    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
+    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (register int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+template <typename T>
+inline T read() {
+    register T x = 0;
+    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
+        if (c == '-') f = -1;
+    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
+    return x * f;
+}
+template <typename T>
+inline void write(const T& t) {
+    register T x = t;
+    if (!x) putch('0');
+    if (x < 0) putch('-'), x = -x;
+    while (x) qu[++qr] = x % 10 + '0', x /= 10;
+    while (qr) putch(qu[qr--]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace io
+using io::getch;
+using io::getstr;
+using io::putch;
+using io::putstr;
+using io::read;
+using io::write;
+
+#define maxn 1000005
+#define mod 998244353
+
+long long fac[maxn], inv[maxn];
+
+long long Pow(long long a, long long x) {
+    long long ans = 1;
+    while (x) {
+        if (x & 1) ans = ans * a % mod;
+        a = a * a % mod;
+        x >>= 1;
+    }
+    return ans;
+}
+
+inline long long C(int n, int m) { return fac[n] * inv[m] % mod * inv[n - m] % mod; }
+
+int main() {
+    fac[0] = inv[0] = 1;
+    for (register int i = 1; i < maxn; i++) fac[i] = fac[i - 1] * i % mod;
+    inv[maxn - 1] = Pow(fac[maxn - 1], mod - 2);
+    for (register int i = maxn - 2; i; i--) inv[i] = inv[i + 1] * (i + 1) % mod;
+    int n = read<int>(), m = read<int>();
+    if (n < m) swap(n, m);
+    long long ans = 0;
+    for (register int i = 1; i <= m; i++) ans = (ans + C(i + i, i) * C(n - i + m - i, n - i) % mod) % mod;
+    write((ans * Pow(C(n + m, m), mod - 2) % mod * Pow(2, mod - 2) % mod + n) % mod), putch('\n');
+    return 0;
+}
