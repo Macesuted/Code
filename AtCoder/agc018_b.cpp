537fb0782b0da6115b84d42e06365386466e305e
Macesuted <macesuted@qq.com>
2021-07-16 15:32:58 +0800

    update

---
+++ AtCoder/agc018_b.cpp
@@ -0,0 +1,83 @@
+/**
+ * @author Macesuted (macesuted@qq.com)
+ * @copyright Copyright (c) 2021
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace io {
+#define SIZE (1 << 20)
+char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
+int f, qr;
+inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
+inline char getch(void) { return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++); }
+inline void putch(char x) {
+    *oS++ = x;
+    if (oS == oT) flush();
+    return;
+}
+string getstr(void) {
+    string s = "";
+    char c = getch();
+    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
+    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (register int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+template <typename T>
+inline T read() {
+    register T x = 0;
+    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
+        if (c == '-') f = -1;
+    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
+    return x * f;
+}
+template <typename T>
+inline void write(const T& t) {
+    register T x = t;
+    if (!x) putch('0');
+    if (x < 0) putch('-'), x = -x;
+    while (x) qu[++qr] = x % 10 + '0', x /= 10;
+    while (qr) putch(qu[qr--]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace io
+using io::getch;
+using io::getstr;
+using io::putch;
+using io::putstr;
+using io::read;
+using io::write;
+
+#define maxn 305
+
+int a[maxn][maxn], cnt[maxn], pos[maxn], del[maxn];
+
+int main() {
+    int n = read<int>(), m = read<int>();
+    for (register int i = 1; i <= n; i++)
+        for (register int j = 1; j <= m; j++)
+            a[i][j] = read<int>();
+    for (register int i = 1; i <= n; i++) pos[i] = 1;
+    int answer = n;
+    for (register int t = 1; t < m; t++) {
+        int mxPos = 0;
+        memset(cnt, 0, sizeof(cnt));
+        for (register int i = 1; i <= n; i++) {
+            while (del[a[i][pos[i]]]) pos[i]++;
+            if (++cnt[a[i][pos[i]]] > cnt[mxPos]) mxPos = a[i][pos[i]];
+        }
+        answer = min(answer, cnt[mxPos]);
+        del[mxPos] = true;
+    }
+    write(answer), putch('\n');
+    return 0;
+}
