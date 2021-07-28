e7157640eb1787f1cac66fc7c810bdcb2990b89f
Macesuted <macesuted@qq.com>
2021-07-28 13:40:13 +0800

    Ynoi!!!

--- Luogu/7447.cpp
+++ Luogu/7447.cpp
@@ -1,6 +1,8 @@
 /**
  * @author Macesuted (macesuted@qq.com)
  * @copyright Copyright (c) 2021
+ * @brief
+ *      My Tutorial: https://www.macesuted.cn/article/lg7447/
  */
 
 #include <bits/stdc++.h>
