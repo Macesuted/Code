54901831e7d870a10dd2fd07f70186111222fbca
Macesuted <macesuted@qq.com>
2021-10-01 07:54:05 +0800

    update

---
+++ Codeforces/1580A.cpp
@@ -0,0 +1,69 @@
+/**
+ * @author Macesuted (macesuted@outlook.com)
+ * @copyright Copyright (c) 2021
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+#define maxn 405
+
+typedef pair<int, int> pii;
+
+int a[maxn][maxn], sum[maxn][maxn];
+
+int SUM(int i1, int j1, int i2, int j2) { return sum[i2][j2] - sum[i2][j1 - 1] - sum[i1 - 1][j2] + sum[i1 - 1][j1 - 1]; }
+int cost(int i1, int j1, int i2, int j2) {
+    return (j2 - j1 - 1 - SUM(i1, j1 + 1, i1, j2 - 1)) +
+           (j2 - j1 - 1 - SUM(i2, j1 + 1, i2, j2 - 1)) +
+           (i2 - i1 - 1 - SUM(i1 + 1, j1, i2 - 1, j1)) +
+           (i2 - i1 - 1 - SUM(i1 + 1, j2, i2 - 1, j2)) +
+           SUM(i1 + 1, j1 + 1, i2 - 1, j2 - 1);
+}
+int limit(int i1, int j1, int i2, int j2) {
+    return (j2 - j1 - 1 - SUM(i2, j1 + 1, i2, j2 - 1)) +
+           (i2 - i1 - 1 - SUM(i1 + 1, j2, i2 - 1, j2));
+}
+
+void work(void) {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            char c;
+            cin >> c;
+            sum[i][j] = a[i][j] = (c == '1');
+        }
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            sum[i][j] += sum[i][j - 1];
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            sum[i][j] += sum[i - 1][j];
+    int answer = 16;
+    for (int i = 5; i <= n; i++)
+        for (int j = 4; j <= m; j++) {
+            if (SUM(i - 3, j - 2, i - 1, j - 1) >= answer) continue;
+            for (int x = i - 4; x; x--) {
+                if (SUM(x + 1, j - 2, i - 1, j - 1) >= answer || limit(x, j - 3, i, j) >= answer) break;
+                for (int y = j - 3; y; y--) {
+                    if (SUM(x + 1, y + 1, i - 1, j - 1) >= answer || limit(x, y, i, j) >= answer) break;
+                    answer = min(answer, cost(x, y, i, j));
+                }
+            }
+        }
+    cout << answer << endl;
+    return;
+}
+
+signed main() {
+    freopen("tat.in", "r", stdin);
+    ios::sync_with_stdio(false);
+    cout << setiosflags(ios::fixed) << setprecision(11);
+    int _ = 1;
+    cin >> _;
+    while (_--) work();
+    return 0;
+}
---
+++ Codeforces/1580B.cpp
@@ -0,0 +1,54 @@
+/**
+ * @author Macesuted (macesuted@outlook.com)
+ * @copyright Copyright (c) 2021
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+#define maxn 105
+
+typedef pair<int, int> pii;
+
+int f[maxn][maxn][maxn];
+int C[maxn][maxn], fac[maxn];
+
+int n, m, k, mod;
+
+int dp(int len, int up, int cnt) {
+    if (!len) return cnt == 0;
+    if (up == 1) return cnt == 1 ? fac[len] : 0;
+    if ((cnt - 1) * 2 > len) return 0;
+    if (f[len][up][cnt] >= 0) return f[len][up][cnt];
+    int answer = 0;
+    for (int mid = 0; mid < len; mid++)
+        for (int t = 0; t <= cnt; t++)
+            answer = (answer +
+                      dp(mid, up - 1, t) * dp(len - mid - 1, up - 1, cnt - t) % mod * C[len - 1][mid]) %
+                     mod;
+    return f[len][up][cnt] = answer;
+}
+
+void work(void) {
+    memset(f, -1, sizeof(f));
+    cin >> n >> m >> k >> mod;
+    fac[0] = 1;
+    for (int i = 1; i < maxn; i++) fac[i] = fac[i - 1] * i % mod;
+    for (int i = 0; i < maxn; i++) {
+        C[i][0] = C[i][i] = 1;
+        for (int j = 1; j < i; j++) C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % mod;
+    }
+    cout << dp(n, m, k) << endl;
+    return;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cout << setiosflags(ios::fixed) << setprecision(11);
+    int _ = 1;
+    // cin >> _;
+    while (_--) work();
+    return 0;
+}
