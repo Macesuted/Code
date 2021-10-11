5957da2455ec5d1bd17c5822cb2e1d44ee616a4b
Macesuted <macesuted@qq.com>
2021-10-11 19:38:31 +0800

    HydroOJ: H1066

---
+++ HydroOJ/H1066.cpp
@@ -0,0 +1,95 @@
+/**
+ * @author Macesuted (i@macesuted.moe)
+ * @copyright Copyright (c) 2021
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace io {
+#define SIZE (1 << 20)
+char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
+int f, qr;
+inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
+inline char getch(void) { return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++); }
+inline void putch(char x) {
+    *oS++ = x;
+    if (oS == oT) flush();
+    return;
+}
+string getstr(void) {
+    string s = "";
+    char c = getch();
+    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
+    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (register int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+template <typename T>
+inline T read() {
+    register T x = 0;
+    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
+        if (c == '-') f = -1;
+    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
+    return x * f;
+}
+template <typename T>
+inline void write(const T& t) {
+    register T x = t;
+    if (!x) putch('0');
+    if (x < 0) putch('-'), x = -x;
+    while (x) qu[++qr] = x % 10 + '0', x /= 10;
+    while (qr) putch(qu[qr--]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace io
+using io::getch;
+using io::getstr;
+using io::putch;
+using io::putstr;
+using io::read;
+using io::write;
+
+#define maxn 100005
+
+typedef pair<long long, int> pli;
+
+long long dist[maxn];
+bool vis[maxn];
+
+vector<vector<pli>> graph;
+
+void Dijkstra(int S) {
+    memset(dist, 0x3f, sizeof(dist));
+    static priority_queue<pli, vector<pli>, greater<pli>> que;
+    que.emplace(dist[S] = 0, S);
+    while (!que.empty()) {
+        int p = que.top().second;
+        que.pop();
+        if (vis[p]) continue;
+        vis[p] = true;
+        for (auto& i : graph[p])
+            if (dist[i.second] > dist[p] + i.first)
+                que.emplace(dist[i.second] = dist[p] + i.first, i.second);
+    }
+    return;
+}
+
+int main() {
+    int n = read<int>(), m = read<int>(), s = read<int>();
+    graph.resize(n + 1);
+    for (int i = 1; i <= m; i++) {
+        int from = read<int>(), to = read<int>(), dist = read<int>();
+        graph[from].emplace_back(dist, to);
+    }
+    Dijkstra(s);
+    for (int i = 1; i <= n; i++) write(dist[i]), putch(" \n"[i == n]);
+    return 0;
+}
