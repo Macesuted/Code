7a6e84c1c8cb9f31fc4e322ee95c4e61b2a370d8
Macesuted <macesuted@qq.com>
2021-10-29 12:53:37 +0800

    Codeforces 1575L: Longest Array Deconstruction

---
+++ Codeforces/1575L.cpp
@@ -0,0 +1,106 @@
+/**
+ * @file 1575L.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2021-10-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace io {
+#define SIZE (1 << 20)
+char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
+int f, qr;
+inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
+inline char getch(void) {
+    return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++);
+}
+inline void putch(char x) {
+    *oS++ = x;
+    if (oS == oT) flush();
+    return;
+}
+string getstr(void) {
+    string s = "";
+    char c = getch();
+    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
+    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (register int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+template <typename T>
+inline T read() {
+    register T x = 0;
+    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
+        if (c == '-') f = -1;
+    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
+    return x * f;
+}
+template <typename T>
+inline void write(const T& t) {
+    register T x = t;
+    if (!x) putch('0');
+    if (x < 0) putch('-'), x = -x;
+    while (x) qu[++qr] = x % 10 + '0', x /= 10;
+    while (qr) putch(qu[qr--]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace io
+using io::getch;
+using io::getstr;
+using io::putch;
+using io::putstr;
+using io::read;
+using io::write;
+
+bool mem1;
+
+#define maxn 200005
+
+typedef pair<int, int> pii;
+
+pii a[maxn];
+int S[maxn];
+
+void solve(void) {
+    int n = read<int>();
+    for (int i = 1; i <= n; i++) {
+        int x = read<int>();
+        a[i] = {i - x, x};
+    }
+    sort(a + 1, a + n + 1);
+    int answer = 0;
+    memset(S, 0x3f, sizeof(S));
+    for (int i = 1; i <= n; i++) {
+        if (a[i].first < 0) continue;
+        int x = a[i].second, p = lower_bound(S + 1, S + answer + 1, x) - S;
+        S[p] = min(S[p], x);
+        answer = max(answer, p);
+    }
+    write(answer), putch('\n');
+    return;
+}
+
+bool mem2;
+
+int main() {
+    clock_t t1 = clock();
+    cerr << "Memory: " << abs(&mem1 - &mem2) / 1024.0 / 1024.0 << "MB" << endl;
+
+    int _ = 1;
+    while (_--) solve();
+
+    clock_t t2 = clock();
+    cerr << "Time: " << (t2 - t1) * 1000.0 / CLOCKS_PER_SEC << "ms" << endl;
+    return 0;
+}
