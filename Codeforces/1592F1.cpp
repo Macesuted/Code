93d201a7d6f37b8fc42818cc47f9b7eabe2fb8be
Macesuted <i@macesuted.moe>
2022-02-25 08:08:02 +0800

    Codeforces: 1592F1 1592F2 1592F1: Alice and Recoloring 1 1592F2: Alice and Recoloring 2

---
+++ Codeforces/1592F1.cpp
@@ -0,0 +1,109 @@
+/**
+ * @file 1592F2.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-02-25
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MP make_pair
+#define MT make_tuple
+
+namespace io {
+#define SIZE (1 << 20)
+char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
+int f, qr;
+inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
+inline char getch(void) {
+    return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++);
+}
+void putch(char x) {
+    *oS++ = x;
+    if (oS == oT) flush();
+    return;
+}
+string getstr(void) {
+    string s = "";
+    char c = getch();
+    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
+    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+template <typename T>
+T read() {
+    T x = 0;
+    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
+        if (c == '-') f = -1;
+    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
+    return x * f;
+}
+template <typename T>
+void write(const T& t) {
+    T x = t;
+    if (!x) putch('0');
+    if (x < 0) putch('-'), x = -x;
+    while (x) qu[++qr] = x % 10 + '0', x /= 10;
+    while (qr) putch(qu[qr--]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace io
+using io::getch;
+using io::getstr;
+using io::putch;
+using io::putstr;
+using io::read;
+using io::write;
+
+bool mem1;
+
+#define maxn 505
+
+int a[maxn][maxn], b[maxn][maxn];
+
+void solve(void) {
+    int n = read<int>(), m = read<int>();
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            char c = getch();
+            while (c != 'W' && c != 'B') c = getch();
+            a[i][j] = (c == 'B');
+        }
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) b[i][j] = a[i][j] ^ a[i][j + 1] ^ a[i + 1][j] ^ a[i + 1][j + 1];
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            ans += b[i][j];
+            if (i != n && j != m && b[i][j] && b[i][m] && b[n][j] && b[n][m]) b[n][m] = 0;
+        }
+    write(ans), putch('\n');
+    return;
+}
+
+bool mem2;
+
+int main() {
+#ifdef MACESUTED
+    cerr << "Memory: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef MACESUTED
+    cerr << "Time: " << clock() * 1000. / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
