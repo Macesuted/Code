475bfb543e6dc51ba828bf5e6462ba01935bc4e6
Macesuted <i@macesuted.moe>
2022-03-01 19:55:21 +0800

    USACO: Feb. 2022 Ag

---
+++ USACO/1195.cpp
@@ -0,0 +1,144 @@
+/**
+ * @file 1195.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-02-26
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MP make_pair
+#define MT make_tuple
+
+namespace io {
+#define SIZE (1 << 20)
+char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
+int f, qr;
+inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
+inline char getch(void) {
+    return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++);
+}
+void putch(char x) {
+    *oS++ = x;
+    if (oS == oT) flush();
+    return;
+}
+string getstr(void) {
+    string s = "";
+    char c = getch();
+    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
+    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+template <typename T>
+T read() {
+    T x = 0;
+    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
+        if (c == '-') f = -1;
+    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
+    return x * f;
+}
+template <typename T>
+void write(const T& t) {
+    T x = t;
+    if (!x) putch('0');
+    if (x < 0) putch('-'), x = -x;
+    while (x) qu[++qr] = x % 10 + '0', x /= 10;
+    while (qr) putch(qu[qr--]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace io
+using io::getch;
+using io::getstr;
+using io::putch;
+using io::putstr;
+using io::read;
+using io::write;
+
+bool mem1;
+
+#define maxn 45
+#define maxhn 22
+
+typedef pair<long long, long long> pll;
+
+struct Node {
+    long long x, y;
+    int dep, use;
+    inline bool operator<(const Node& oth) const { return x < oth.x || (x == oth.x && y < oth.y); }
+};
+
+vector<Node> S;
+
+int a[maxn], b[maxn];
+int cache[1 << maxhn], tail;
+long long ans[maxn];
+int cnt[maxn];
+
+void solve(void) {
+    int n = read<int>(), X = read<int>(), Y = read<int>(), n2 = n >> 1;
+    for (int i = 1; i <= n; i++) a[i] = read<int>(), b[i] = read<int>();
+    static queue<Node> que;
+    while (!que.empty()) que.pop();
+    que.push(Node{0, 0, 1, 0});
+    while (!que.empty()) {
+        Node p = que.front();
+        que.pop();
+        if (p.dep == n2 + 1)
+            S.push_back(p);
+        else
+            que.push(Node{p.x, p.y, p.dep + 1, p.use}), que.push(Node{p.x + a[p.dep], p.y + b[p.dep], p.dep + 1, p.use + 1});
+    }
+    que.push(Node{X, Y, n, 0});
+    while (!que.empty()) {
+        Node p = que.front();
+        que.pop();
+        if (p.dep == n2)
+            S.push_back(p);
+        else
+            que.push(Node{p.x, p.y, p.dep - 1, p.use}), que.push(Node{p.x - a[p.dep], p.y - b[p.dep], p.dep - 1, p.use + 1});
+    }
+    sort(S.begin(), S.end());
+    for (auto i = S.begin(), j = i; i < S.end(); i = j) {
+        j = i;
+        while (j < S.end() && j->x == i->x && j->y == i->y) j++;
+        tail = 0;
+        for (auto k = i; k < j; k++)
+            if (k->dep == n2)
+                cache[tail++] = k->use;
+            else
+                cnt[k->use]++;
+        for (int i = 0; i < tail; i++)
+            for (int j = 0; j <= n2; j++) ans[cache[i] + j] += cnt[j];
+        for (int i = 0; i <= n2; i++) cnt[i] = 0;
+    }
+    for (int i = 1; i <= n; i++) write(ans[i]), putch('\n');
+    return;
+}
+
+bool mem2;
+
+int main() {
+#ifdef MACESUTED
+    cerr << "Memory: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef MACESUTED
+    cerr << "Time: " << clock() * 1000. / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
---
+++ USACO/1196.cpp
@@ -0,0 +1,132 @@
+/**
+ * @file 1196.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-02-26
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MP make_pair
+#define MT make_tuple
+
+namespace io {
+#define SIZE (1 << 20)
+char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
+int f, qr;
+inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
+inline char getch(void) {
+    return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++);
+}
+void putch(char x) {
+    *oS++ = x;
+    if (oS == oT) flush();
+    return;
+}
+string getstr(void) {
+    string s = "";
+    char c = getch();
+    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
+    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+template <typename T>
+T read() {
+    T x = 0;
+    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
+        if (c == '-') f = -1;
+    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
+    return x * f;
+}
+template <typename T>
+void write(const T& t) {
+    T x = t;
+    if (!x) putch('0');
+    if (x < 0) putch('-'), x = -x;
+    while (x) qu[++qr] = x % 10 + '0', x /= 10;
+    while (qr) putch(qu[qr--]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace io
+using io::getch;
+using io::getstr;
+using io::putch;
+using io::putstr;
+using io::read;
+using io::write;
+
+bool mem1;
+
+#define maxn 100005
+#define maxm 10005
+
+int a[maxn], lef[maxn], rig[maxn];
+bool flag[maxn];
+vector<int> rec[maxm];
+queue<int> newAdd;
+
+void del(int p, int& l, int& r) {
+    if (!flag[p]) return;
+    flag[p] = false;
+    while (p > r) l = rig[l], r = rig[r];
+    if (l == p) l = rig[p];
+    if (r == p) r = lef[p];
+    rig[lef[p]] = rig[p], lef[rig[p]] = lef[p];
+    if (!rig[r]) {
+        if (lef[l]) newAdd.push(l = lef[l]);
+    } else
+        newAdd.push(r = rig[r]);
+    return;
+}
+
+void solve(void) {
+    int m = read<int>(), n = read<int>(), k = read<int>();
+    for (int i = 1; i <= m; i++) rec[i].clear();
+    for (int i = 1; i <= n; i++) rec[a[i] = read<int>()].push_back(i), lef[i] = i - 1, rig[i] = i + 1, flag[i] = true;
+    rig[n] = 0;
+    int l = 1, r = k;
+    for (int i = 1; i <= k; i++) newAdd.push(i);
+    for (int liml = 1, limr = k; liml <= m; liml++, limr++) {
+        for (auto i : rec[limr])
+            if (flag[i] && l <= i && i <= r) del(i, l, r);
+        while (!newAdd.empty()) {
+            int p = newAdd.front();
+            newAdd.pop();
+            if (liml <= a[p] && a[p] <= limr) del(p, l, r);
+        }
+        for (int i : rec[liml])
+            if (flag[i]) {
+                if (i < l) return putstr("NO\n");
+                while (i > r) l = rig[l], r = rig[r];
+                del(i, l, r);
+            }
+    }
+    return putstr("YES\n");
+}
+
+bool mem2;
+
+int main() {
+#ifdef MACESUTED
+    cerr << "Memory: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = read<int>();
+    while (_--) solve();
+
+#ifdef MACESUTED
+    cerr << "Time: " << clock() * 1000. / CLOCKS_PER_SEC << "ms" << endl;
+#endif
+    return 0;
+}
