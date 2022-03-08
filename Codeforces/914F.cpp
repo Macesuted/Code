dc56e21cfe41ab8fd332d8cfa38e13c27e7021b8
Macesuted <i@macesuted.moe>
2022-03-08 16:31:21 +0800

    Codeforces: 914F Substrings in a String

---
+++ Codeforces/914F.cpp
@@ -0,0 +1,111 @@
+/**
+ * @file 914F.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-03-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace io {
+const int SIZE = 1 << 20;
+char Ibuf[SIZE], *Il = Ibuf, *Ir = Ibuf, Obuf[SIZE], *Ol = Obuf, *Or = Ol + SIZE - 1, stack[32];
+char isspace(char c) { return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r'; }
+void fill(void) { return Ir = (Il = Ibuf) + fread(Ibuf, 1, SIZE, stdin), void(); }
+void flush(void) { return fwrite(Obuf, 1, Ol - Obuf, stdout), Ol = Obuf, void(); }
+char buftop(void) { return Ir == Il ? fill(), *Il : *Il; }
+char getch(void) { return Il == Ir ? fill(), Il == Ir ? EOF : *Il++ : *Il++; }
+void putch(char x) { return *Ol++ = x, Ol == Or ? flush() : void(); }
+template <typename T>
+T read(void) {
+    T x = 0, f = +1;
+    char c = getch();
+    while (c < '0' || c > '9') c == '-' ? void(f = -f) : void(), c = getch();
+    while ('0' <= c && c <= '9') x = (x << 3) + (x << 1) + (c ^ 48), c = getch();
+    return x * f;
+}
+template <typename T>
+void write(T x) {
+    if (!x) putch('0');
+    if (x < 0) putch('-'), x = -x;
+    int top = 0;
+    while (x) stack[top++] = (x % 10) ^ 48, x /= 10;
+    while (top) putch(stack[--top]);
+    return;
+}
+string getstr(const string& suf = "") {
+    string s = suf;
+    while (isspace(buftop()) && buftop() != NULL) getch();
+    if (buftop == NULL) return s;
+    while (Il != Ir) {
+        char* p = Il;
+        while (Il < Ir && !isspace(*Il) && *Il != EOF) Il++;
+        s.append(p, Il);
+        if (Il < Ir) break;
+        fill();
+    }
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace io
+using io::getch;
+using io::getstr;
+using io::putch;
+using io::putstr;
+using io::read;
+using io::write;
+
+bool mem1;
+
+#define maxn 100005
+
+bitset<maxn> S[26], maxS, T;
+
+void solve(void) {
+    string s = getstr();
+    int n = s.size();
+    for (int i = 0; i < n; i++) S[s[i] - 'a'][i] = maxS[i] = true;
+    int q = read<int>();
+    while (q--) {
+        int op = read<int>();
+        if (op == 1) {
+            int i = read<int>() - 1;
+            char c = getch();
+            while (c < 'a' || c > 'z') c = getch();
+            S[s[i] - 'a'][i] = false, S[(s[i] = c) - 'a'][i] = true;
+        } else {
+            int l = read<int>() - 1, r = read<int>() - 1;
+            string t = getstr();
+            T = maxS >> (n - (r - l + 2 - t.size()));
+            for (int i = 0; i < (int)t.size(); i++) T &= S[t[i] - 'a'] >> (l + i);
+            write(T.count()), putch('\n');
+        }
+    }
+    return;
+}
+
+bool mem2;
+
+int main() {
+#ifdef MACESUTED
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef MACESUTED
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
