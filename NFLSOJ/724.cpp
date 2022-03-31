d1d420706bffd21bd39d08aea4fff9cefdde8111
Macesuted <i@macesuted.moe>
2022-03-31 12:42:33 +0800

    NFLSOJ: 724 宝藏

---
+++ NFLSOJ/724.cpp
@@ -0,0 +1,159 @@
+/**
+ * @file 724.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-03-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace IO {
+const int SIZE = 1 << 20;
+char Ibuf[SIZE], *Il = Ibuf, *Ir = Ibuf, Obuf[SIZE], *Ol = Obuf, *Or = Ol + SIZE - 1;
+int cache1[100], cache2[100];
+char isspace(char c) { return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r'; }
+char iseoln(char c) { return c == '\n' || c == '\r'; }
+void fill(void) { return Ir = (Il = Ibuf) + fread(Ibuf, 1, SIZE, stdin), void(); }
+void flush(void) { return fwrite(Obuf, 1, Ol - Obuf, stdout), Ol = Obuf, void(); }
+char buftop(void) { return Ir == Il && (fill(), 1), *Il; }
+char getch(void) { return Il == Ir ? fill(), Il == Ir ? EOF : *Il++ : *Il++; }
+void putch(char x) { return *Ol++ = x, Ol == Or && (flush(), 1), void(); }
+template <typename T = int>
+T read(void) {
+    T x = 0, f = +1;
+    char c = getch();
+    while (c < '0' || c > '9') (c == '-') && (f = -f), c = getch();
+    while ('0' <= c && c <= '9') x = (x << 3) + (x << 1) + (c ^ 48), c = getch();
+    return x * f;
+}
+template <typename T>
+void write(T x) {
+    if (!x) return putch('0');
+    if (x < 0) putch('-'), x = -x;
+    int top = 0;
+    while (x) cache1[top++] = x % 10, x /= 10;
+    while (top) putch(cache1[--top] ^ 48);
+    return;
+}
+template <typename T>
+void writeDouble(T x, int dep = 10) {
+    if (x < 0) putch('-'), x = -x;
+    __int128 fx = x;
+    x -= fx;
+    for (int i = 0; i < dep; i++) cache2[i] = (x *= 10), x -= int(x);
+    if (int(x * 10) > 4) {
+        cache2[dep - 1]++;
+        for (int i = dep - 1; i; i--)
+            if (cache2[i] == 10) cache2[i] = 0, cache2[i - 1]++;
+        if (cache2[0] == 10) cache2[0] = 0, fx++;
+    }
+    write(fx), putch('.');
+    for (int i = 0; i < dep; i++) putch(cache2[i] ^ 48);
+    return;
+}
+string getstr(const string& suf = "") {
+    string s = suf;
+    while (isspace(buftop())) getch();
+    for (char* p = Il; Il != Ir; fill(), p = Il) {
+        while (Il < Ir && !isspace(*Il) && *Il != EOF) Il++;
+        s.append(p, Il);
+        if (Il < Ir) break;
+    }
+    return s;
+}
+string getline(const string& suf = "") {
+    string s = suf;
+    while (iseoln(buftop())) getch();
+    for (char* p = Il; Il != Ir; fill(), p = Il) {
+        while (Il < Ir && !iseoln(*Il) && *Il != EOF) Il++;
+        s.append(p, Il);
+        if (Il < Ir) break;
+    }
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace IO
+using IO::getch;
+using IO::getline;
+using IO::getstr;
+using IO::putch;
+using IO::putstr;
+using IO::read;
+using IO::write;
+using IO::writeDouble;
+
+bool mem1;
+
+typedef pair<int, int> pii;
+
+unordered_map<int64_t, int64_t> F;
+
+vector<pii> lim[45][2];
+bool v[45];
+
+int64_t f(int64_t);
+int64_t calc(int64_t S, int x, bool val) {
+    int64_t T = S;
+    static queue<int> que;
+    while (!que.empty()) que.pop();
+    v[x] = val, que.push(x), T ^= 1LL << x;
+    while (!que.empty()) {
+        int p = que.front();
+        que.pop();
+        for (auto i : lim[p][v[p]]) {
+            if (!(S >> i.first & 1)) continue;
+            if (!(T >> i.first & 1) && v[i.first] != i.second) return 0;
+            if (T >> i.first & 1) v[i.first] = i.second, que.push(i.first), T ^= 1LL << i.first;
+        }
+    }
+    return f(T);
+}
+int64_t f(int64_t S) {
+    if (S == 0) return 1;
+    if (F.find(S) != F.end()) return F[S];
+    int p = 0;
+    while ((1LL << p) != (S & -S)) p++;
+    F[S] += calc(S, p, false) + calc(S, p, true);
+    return F[S];
+}
+
+void solve(void) {
+    int n = read(), m1 = read(), m2 = read();
+    for (int i = 1; i <= m1; i++) {
+        int x = read() - 1, y = read() - 1;
+        lim[x][1].emplace_back(y, 1), lim[y][0].emplace_back(x, 0);
+    }
+    for (int i = 1; i <= m2; i++) {
+        int x = read() - 1, y = read() - 1;
+        lim[x][1].emplace_back(y, 0), lim[y][1].emplace_back(x, 0);
+    }
+    write(f((1LL << n) - 1)), putch('\n');
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifdef MACESUTED
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef MACESUTED
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
