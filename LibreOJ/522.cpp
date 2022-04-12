c25d1fbb536f6789bc77e1c5c5c0cab65b51cb0b
Macesuted <i@macesuted.moe>
2022-04-12 13:47:40 +0800

    LibreOJ: 522 「LibreOJ β Round #3」绯色 IOI（危机）

---
+++ LibreOJ/522.cpp
@@ -0,0 +1,151 @@
+/**
+ * @file 522.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-04-12
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace IO {
+const int SIZE = 1 << 20;
+char Ibuf[SIZE], *Il = Ibuf, *Ir = Ibuf, Obuf[SIZE], *Ol = Obuf, *Or = Ol + SIZE - 1;
+int cache1[100], cache2[100];
+char isspace(char c) { return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r'; }
+char iseoln(char c) { return c == '\n' || c == '\r'; }
+void fill(void) { return Ir = (Il = Ibuf) + fread(Ibuf, 1, SIZE, stdin), void(); }
+void flush(void) { return fwrite(Obuf, 1, Ol - Obuf, stdout), Ol = Obuf, void(); }
+char buftop(void) { return Ir == Il && (fill(), 1), *Il; }
+char getch(void) { return Il == Ir ? fill(), Il == Ir ? EOF : *Il++ : *Il++; }
+void putch(char x) { return *Ol++ = x, Ol == Or && (flush(), 1), void(); }
+template <typename T = int>
+T read(void) {
+    T x = 0, f = +1;
+    char c = getch();
+    while (c < '0' || c > '9') (c == '-') && (f = -f), c = getch();
+    while ('0' <= c && c <= '9') x = (x << 3) + (x << 1) + (c ^ 48), c = getch();
+    return x * f;
+}
+template <typename T>
+void write(T x) {
+    if (!x) return putch('0');
+    if (x < 0) putch('-'), x = -x;
+    int top = 0;
+    while (x) cache1[top++] = x % 10, x /= 10;
+    while (top) putch(cache1[--top] ^ 48);
+    return;
+}
+template <typename T>
+void writeDouble(T x, int dep = 10) {
+    if (x < 0) putch('-'), x = -x;
+    int64_t fx = x;
+    x -= fx;
+    for (int i = 0; i < dep; i++) cache2[i] = (x *= 10), x -= int(x);
+    if (int(x * 10) > 4) {
+        cache2[dep - 1]++;
+        for (int i = dep - 1; i; i--)
+            if (cache2[i] == 10) cache2[i] = 0, cache2[i - 1]++;
+        if (cache2[0] == 10) cache2[0] = 0, fx++;
+    }
+    write(fx), putch('.');
+    for (int i = 0; i < dep; i++) putch(cache2[i] ^ 48);
+    return;
+}
+string getstr(const string& suf = "") {
+    string s = suf;
+    while (isspace(buftop())) getch();
+    for (char* p = Il; Il != Ir; fill(), p = Il) {
+        while (Il < Ir && !isspace(*Il) && *Il != EOF) Il++;
+        s.append(p, Il);
+        if (Il < Ir) break;
+    }
+    return s;
+}
+string getline(const string& suf = "") {
+    string s = suf;
+    while (iseoln(buftop())) getch();
+    for (char* p = Il; Il != Ir; fill(), p = Il) {
+        while (Il < Ir && !iseoln(*Il) && *Il != EOF) Il++;
+        s.append(p, Il);
+        if (Il < Ir) break;
+    }
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace IO
+using IO::getch;
+using IO::getline;
+using IO::getstr;
+using IO::putch;
+using IO::putstr;
+using IO::read;
+using IO::write;
+using IO::writeDouble;
+
+bool mem1;
+
+#define maxn 300005
+#define mod 998244353
+
+typedef tuple<int64_t, int64_t, int, int> tllii;
+
+int deg[maxn];
+int64_t f[maxn], ans[maxn];
+tllii a[maxn];
+vector<int> graph[maxn];
+
+void solve(void) {
+    int n = read();
+    for (int i = 1; i <= n; i++) get<0>(a[i]) = read<int64_t>();
+    for (int i = 1; i <= n; i++) get<1>(a[i]) = read<int64_t>();
+    for (int i = 1; i <= n; i++) get<2>(a[i]) = read(), get<3>(a[i]) = i;
+    sort(a + 1, a + n + 1);
+    for (int i = 1; i <= n; i++) {
+        for (int64_t j = i - 1, l = get<0>(a[i]) - get<1>(a[i]); j >= 1 && l <= get<0>(a[j]); j--)
+            graph[i].push_back(j), deg[j]++, l = min(l, get<0>(a[j]) - get<1>(a[j]));
+        for (int64_t j = i + 1, r = get<0>(a[i]) + get<1>(a[i]); j <= n && r >= get<0>(a[j]); j++)
+            graph[i].push_back(j), deg[j]++, r = max(r, get<0>(a[j]) + get<1>(a[j]));
+    }
+    static queue<int> que;
+    for (int i = 1; i <= n; i++)
+        if (!deg[i]) que.push(i);
+    auto F = [](int64_t x, int64_t y) { return ((x ^ y) + x * y) % mod; };
+    while (!que.empty()) {
+        int p = que.front();
+        que.pop();
+        for (auto i : graph[p]) {
+            f[i] = max(f[i], f[p] + F(get<2>(a[p]), get<2>(a[i])));
+            if (!--deg[i]) que.push(i);
+        }
+    }
+    for (int i = 1; i <= n; i++) ans[get<3>(a[i])] = f[i];
+    for (int i = 1; i <= n; i++) write(ans[i]), putch('\n');
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifdef MACESUTED
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef MACESUTED
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
