8183f8697767a3466faa672c132efec7cc9f5e45
Macesuted <i@macesuted.moe>
2022-04-27 18:21:15 +0800

    HLOJ/UOJ version: 3948 nim

---
+++ HLOJ/UOJ version/3948.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file 3948.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-04-27
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool mem1;
+
+#define maxn 100005
+#define mod 998244353
+
+int a[maxn];
+
+void solve(void) {
+    int n, t = INT_MAX;
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i], t = min(t, a[i] & -a[i]);
+    int minVal = INT_MAX;
+    for (int i = 1; i <= n; i++)
+        if ((a[i] & -a[i]) == t) minVal = min(minVal, a[i] / t);
+    cout << minVal / 2 + 1 << endl;
+    return;
+}
+
+bool mem2;
+
+int main() {
+#ifndef MACESUTED
+    freopen("nim.in", "r", stdin), freopen("nim.out", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+#ifdef MACESUTED
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    cin >> _;
+    while (_--) solve();
+
+#ifdef MACESUTED
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
