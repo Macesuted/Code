6c0596f7ff8cb937853309469dcf6598902b6aed
Macesuted <i@macesuted.moe>
2022-05-01 09:17:53 +0800

    move libs

--- tools/windows/compile.bat
+++ tools/windows/compile.bat
@@ -1,2 +1,2 @@
 @echo off
-g++ "%1.cpp" -o "%1.exe" -lm -Wall -Wextra -O2 -std=c++20 "-Wl,--stack=512000000" -I E:\Code\tools\libs\atcoder -I E:\Code\tools\libs\testlib -DMACESUTED
+g++ "%1.cpp" -o "%1.exe" -lm -Wall -Wextra -O2 -std=c++20 "-Wl,--stack=512000000" -I E:\Code\libs\ac-library -I E:\Code\libs\testlib -DLOCAL
