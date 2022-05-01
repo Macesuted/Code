9eb3155bd752bc09315ff06678c86a6b864d5481
Macesuted <i@macesuted.moe>
2022-05-01 09:26:44 +0800

    LibreOJ: 101 最大流

--- LibreOJ/101.cpp
+++ LibreOJ/101.cpp
@@ -1,146 +1,93 @@
 /**
  * @file 101.cpp
  * @author Macesuted (i@macesuted.moe)
- * @date 2021-11-02
- * 
- * @copyright Copyright (c) 2021
- * 
+ * @date 2022-05-01
+ *
+ * @copyright Copyright (c) 2022
+ *
  */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-namespace io {
-#define SIZE (1 << 20)
-char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
-int f, qr;
-inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
-inline char getch(void) {
-    return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++);
-}
-inline void putch(char x) {
-    *oS++ = x;
-    if (oS == oT) flush();
-    return;
-}
-string getstr(void) {
-    string s = "";
-    char c = getch();
-    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
-    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
-    return s;
-}
-void putstr(string str, int begin = 0, int end = -1) {
-    if (end == -1) end = str.size();
-    for (register int i = begin; i < end; i++) putch(str[i]);
-    return;
-}
-template <typename T>
-inline T read() {
-    register T x = 0;
-    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
-        if (c == '-') f = -1;
-    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
-    return x * f;
-}
-template <typename T>
-inline void write(const T& t) {
-    register T x = t;
-    if (!x) putch('0');
-    if (x < 0) putch('-'), x = -x;
-    while (x) qu[++qr] = x % 10 + '0', x /= 10;
-    while (qr) putch(qu[qr--]);
-    return;
-}
-struct Flusher_ {
-    ~Flusher_() { flush(); }
-} io_flusher_;
-}  // namespace io
-using io::getch;
-using io::getstr;
-using io::putch;
-using io::putstr;
-using io::read;
-using io::write;
-
 bool mem1;
 
-template <typename flowType>
-class Network {
+class Dinic {
    private:
     struct Edge {
-        int to;
-        flowType cap, flow;
-        int rev;
+        int to, rev;
+        int64_t cap;
     };
 
     vector<vector<Edge>> graph;
+    vector<vector<Edge>::iterator> cur;
     vector<int> dist;
-    vector<typename vector<Edge>::iterator> cur;
     queue<int> que;
     int n, S, T;
 
     bool bfs(void) {
-        for (int i = 1; i <= n; i++) dist[i] = 0, cur[i] = graph[i].begin();
-        que.push(S), dist[S] = 1;
+        for (int i = 1; i <= n; i++) dist[i] = INT_MAX, cur[i] = graph[i].begin();
+        que.push(S), dist[S] = 0;
         while (!que.empty()) {
             int p = que.front();
             que.pop();
             for (auto i : graph[p])
-                if (dist[i.to] == 0 && i.flow < i.cap)
-                    dist[i.to] = dist[p] + 1, que.push(i.to);
+                if (i.cap && dist[i.to] > dist[p] + 1) dist[i.to] = dist[p] + 1, que.push(i.to);
         }
-        return dist[T];
+        return dist[T] < INT_MAX;
     }
-    flowType dfs(int p, flowType flow) {
-        if (p == T || flow == 0) return flow;
-        flowType rest = flow, c;
-        for (auto i = cur[p]; i != graph[p].end() && rest; i++) {
+    int64_t dfs(int p, int64_t rest) {
+        if (p == T || !rest) return rest;
+        int64_t use = 0, c;
+        for (auto i = cur[p]; i != graph[p].end() && rest > use; i++) {
             cur[p] = i;
-            if (i->cap == i->flow || dist[i->to] != dist[p] + 1) continue;
-            if (!(c = dfs(i->to, min(rest, i->cap - i->flow)))) dist[i->to] = 0;
-            i->flow += c, graph[i->to][i->rev].flow -= c, rest -= c;
+            if (!i->cap || dist[i->to] != dist[p] + 1) continue;
+            if (!(c = dfs(i->to, min(rest - use, i->cap)))) dist[i->to] = -1;
+            use += c, i->cap -= c, graph[i->to][i->rev].cap += c;
         }
-        return flow - rest;
+        return use;
     }
 
    public:
-    inline void resize(int _n) { return n = _n, graph.resize(n + 1), dist.resize(n + 1), cur.resize(n + 1), void(); }
-    inline void addEdge(int from, int to, flowType cap) {
-        return graph[from].push_back(Edge{to, cap, (flowType)0, (int)graph[to].size()}),
-               graph[to].push_back(Edge{from, cap, cap, (int)graph[from].size() - 1});
+    void resize(int _n) { return n = _n, graph.resize(n + 1), dist.resize(n + 1), cur.resize(n + 1); }
+    void addEdge(int from, int to, int cap) {
+        return graph[from].push_back(Edge{to, (int)graph[to].size(), cap}),
+               graph[to].push_back(Edge{from, (int)graph[from].size() - 1, 0});
     }
-    inline flowType maxFlow(int _S, int _T) {
+    int64_t maxFlow(int _S, int _T) {
         S = _S, T = _T;
-        flowType answer = 0;
-        while (bfs()) answer += dfs(S, numeric_limits<flowType>::max());
-        return answer;
+        int64_t ans = 0;
+        while (bfs()) ans += dfs(S, INT64_MAX);
+        return ans;
     }
-};
-
-Network<long long> net;
+} dinic;
 
 void solve(void) {
-    int n = read<int>(), m = read<int>(), S = read<int>(), T = read<int>();
-    net.resize(n);
+    int n, m, s, t;
+    cin >> n >> m >> s >> t;
+    dinic.resize(n);
     for (int i = 1; i <= m; i++) {
-        int from = read<int>(), to = read<int>(), dist = read<int>();
-        net.addEdge(from, to, dist);
+        int x, y, c;
+        cin >> x >> y >> c;
+        dinic.addEdge(x, y, c);
     }
-    write(net.maxFlow(S, T)), putch('\n');
+    cout << dinic.maxFlow(s, t) << endl;
     return;
 }
 
 bool mem2;
 
 int main() {
-    clock_t t1 = clock();
-    cerr << "Memory: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+    ios::sync_with_stdio(false);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
 
     int _ = 1;
     while (_--) solve();
 
-    clock_t t2 = clock();
-    cerr << "Time: " << (t2 - t1) * 1000. / CLOCKS_PER_SEC << "ms" << endl;
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
     return 0;
 }
