640d9a8be68178632393d2998c225186b27e5eda
Macesuted <i@macesuted.moe>
2022-05-20 17:57:04 +0800

    Codeforces: 660E Different Subsets For All Tuples

--- Codeforces/660E.cpp
+++ Codeforces/660E.cpp
@@ -1,78 +1,61 @@
 /**
+ * @file 660E.cpp
  * @author Macesuted (i@macesuted.moe)
- * @copyright Copyright (c) 2021
+ * @date 2022-05-20
+ *
+ * @copyright Copyright (c) 2022
+ *
  */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-namespace io {
-#define SIZE (1 << 20)
-char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
-int f, qr;
-inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
-inline char getch(void) { return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++); }
-inline void putch(char x) {
-    *oS++ = x;
-    if (oS == oT) flush();
-    return;
-}
-string getstr(void) {
-    string s = "";
-    char c = getch();
-    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
-    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) s.push_back(c), c = getch();
-    return s;
-}
-void putstr(string str, int begin = 0, int end = -1) {
-    if (end == -1) end = str.size();
-    for (register int i = begin; i < end; i++) putch(str[i]);
-    return;
-}
-template <typename T>
-inline T read() {
-    register T x = 0;
-    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
-        if (c == '-') f = -1;
-    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
-    return x * f;
-}
-template <typename T>
-inline void write(const T& t) {
-    register T x = t;
-    if (!x) putch('0');
-    if (x < 0) putch('-'), x = -x;
-    while (x) qu[++qr] = x % 10 + '0', x /= 10;
-    while (qr) putch(qu[qr--]);
-    return;
-}
-struct Flusher_ {
-    ~Flusher_() { flush(); }
-} io_flusher_;
-}  // namespace io
-using io::getch;
-using io::getstr;
-using io::putch;
-using io::putstr;
-using io::read;
-using io::write;
+bool mem1;
 
+#define maxn 1000005
 #define mod 1000000007
 
-long long power(long long a, long long x) {
-    long long ans = 1;
+int64_t fac[maxn], ifac[maxn];
+
+int64_t Pow(int64_t a, int64_t x) {
+    int64_t ans = 1;
     while (x) {
         if (x & 1) ans = ans * a % mod;
-        a = a * a % mod;
-        x >>= 1;
+        a = a * a % mod, x >>= 1;
     }
     return ans;
 }
+int64_t binom(int n, int m) { return fac[n] * ifac[m] % mod * ifac[n - m] % mod; }
+
+void solve(void) {
+    int n, m;
+    cin >> n >> m;
+    if (m == 1) return cout << n + 1 << endl, void();
+    int64_t ans = Pow(m, n), invm = Pow(m - 1, mod - 2), pw1 = 1, pw2 = Pow(m - 1, n);
+    for (int i = 1; i <= n; i++)
+        pw1 = pw1 * m % mod, pw2 = pw2 * invm % mod, ans = (ans + pw1 * pw2 % mod * binom(n, n - i + 1)) % mod;
+    cout << ans << endl;
+    return;
+}
+
+bool mem2;
 
 int main() {
-    int n = read<int>(), m = read<int>(), c1 = power(m, mod - 2), c2 = 2 * m - 1;
-    long long a1 = power(m, n), a2 = 1, answer = a1;
-    for (register int i = 1; i <= n; i++) answer = (answer + a1 * a2) % mod, a1 = a1 * c1 % mod, a2 = a2 * c2 % mod;
-    write(answer), putch('\n');
+    ios::sync_with_stdio(false);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    fac[0] = ifac[0] = 1;
+    for (int i = 1; i < maxn; i++) fac[i] = fac[i - 1] * i % mod;
+    ifac[maxn - 1] = Pow(fac[maxn - 1], mod - 2);
+    for (int i = maxn - 2; i; i--) ifac[i] = ifac[i + 1] * (i + 1) % mod;
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
     return 0;
 }
