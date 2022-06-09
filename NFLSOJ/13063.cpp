122e4ddeb8ae4f080e916f889f285e6111f1068c
Macesuted <i@macesuted.moe>
2022-06-09 14:29:20 +0800

    NFLSOJ: 13063 「NOI2022模拟赛0413CJ」生活在图上

---
+++ NFLSOJ/13063.cpp
@@ -0,0 +1,156 @@
+/**
+ * @file 13063.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-06-09
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+bool mem1;
+
+namespace IO {
+const int SIZE = 1 << 24;
+char Ibuf[SIZE], *Il = Ibuf, *Ir = Ibuf, Obuf[SIZE], *Ol = Obuf, *Or = Ol + SIZE - 1;
+int cache1[100], cache2[100];
+char isspace(char c) { return c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r'; }
+char iseoln(char c) { return c == '\n' || c == '\r'; }
+void fill(void) { return Ir = (Il = Ibuf) + fread(Ibuf, 1, SIZE, stdin), void(); }
+void flush(void) { return fwrite(Obuf, 1, Ol - Obuf, stdout), Ol = Obuf, void(); }
+char buftop(void) { return Ir == Il && (fill(), 1), *Il; }
+char getch(void) { return Il == Ir ? fill(), Il == Ir ? EOF : *Il++ : *Il++; }
+void putch(char x) { return *Ol++ = x, Ol == Or && (flush(), 1), void(); }
+template <typename T = int>
+T read(void) {
+    T x = 0, f = +1;
+    char c = getch();
+    while (c < '0' || c > '9') (c == '-') && (f = -f), c = getch();
+    while ('0' <= c && c <= '9') x = (x << 3) + (x << 1) + (c ^ 48), c = getch();
+    return x * f;
+}
+template <typename T>
+void write(T x) {
+    if (!x) return putch('0');
+    if (x < 0) putch('-'), x = -x;
+    int top = 0;
+    while (x) cache1[top++] = x % 10, x /= 10;
+    while (top) putch(cache1[--top] ^ 48);
+    return;
+}
+template <typename T>
+void writeDouble(T x, int dep = 10) {
+    if (x < 0) putch('-'), x = -x;
+    int64_t fx = x;
+    x -= fx;
+    for (int i = 0; i < dep; i++) cache2[i] = (x *= 10), x -= int(x);
+    if (int(x * 10) > 4) {
+        cache2[dep - 1]++;
+        for (int i = dep - 1; i; i--)
+            if (cache2[i] == 10) cache2[i] = 0, cache2[i - 1]++;
+        if (cache2[0] == 10) cache2[0] = 0, fx++;
+    }
+    write(fx), putch('.');
+    for (int i = 0; i < dep; i++) putch(cache2[i] ^ 48);
+    return;
+}
+string getstr(const string& suf = "") {
+    string s = suf;
+    while (isspace(buftop())) getch();
+    for (char* p = Il; Il != Ir; fill(), p = Il) {
+        while (Il < Ir && !isspace(*Il) && *Il != EOF) Il++;
+        s.append(p, Il);
+        if (Il < Ir) break;
+    }
+    return s;
+}
+string getline(const string& suf = "") {
+    string s = suf;
+    while (iseoln(buftop())) getch();
+    for (char* p = Il; Il != Ir; fill(), p = Il) {
+        while (Il < Ir && !iseoln(*Il) && *Il != EOF) Il++;
+        s.append(p, Il);
+        if (Il < Ir) break;
+    }
+    return s;
+}
+void putstr(string str, int begin = 0, int end = -1) {
+    if (end == -1) end = str.size();
+    for (int i = begin; i < end; i++) putch(str[i]);
+    return;
+}
+struct Flusher_ {
+    ~Flusher_() { flush(); }
+} io_flusher_;
+}  // namespace IO
+using IO::getch;
+using IO::getline;
+using IO::getstr;
+using IO::putch;
+using IO::putstr;
+using IO::read;
+using IO::write;
+using IO::writeDouble;
+
+#define maxn 4000005
+#define maxm 20000005
+
+int fa[maxn], siz[maxn], to[maxm], nxt[maxm], head[maxn], ecnt = 0;
+bool vis[maxn];
+
+void addEdge(int x, int y) { return to[++ecnt] = y, nxt[ecnt] = head[x], head[x] = ecnt, siz[x]++, void(); }
+
+int cnt1 = 0, cnt2 = 0;
+void dfs(int p, int pre = -1) {
+    fa[p] = pre, vis[p] = true, cnt1 += (int)siz[p] % 2;
+    bool rec = false;
+    for (int ei = head[p]; ei; ei = nxt[ei]) {
+        int i = to[ei];
+        if (!fa[i])
+            dfs(i, p);
+        else if (vis[i]) {
+            if (i == pre && !rec) {
+                rec = true;
+                continue;
+            }
+            int tot = (siz[i] >= 3), x = p;
+            while (x != i) tot += (siz[x] >= 3), x = fa[x];
+            cnt2 += (tot < 2);
+        }
+    }
+    vis[p] = false;
+    return;
+}
+
+void solve(void) {
+    int n = read(), m = read();
+    for (int i = 1, x, y; i <= m; i++) x = read(), y = read(), addEdge(x, y), addEdge(y, x);
+    dfs(1), write(cnt1 / 2 + cnt2), putch('\n');
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifndef LOCAL
+    freopen("graph.in", "r", stdin), freopen("graph.out", "w", stdout);
+#endif
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
