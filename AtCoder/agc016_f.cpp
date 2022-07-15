ff5a798bdd3de06518c81a11bdf9b2d2891e6738
Macesuted <i@macesuted.moe>
2022-07-15 10:44:50 +0800

    AtCoder: agc016_f Games on DAG

--- AtCoder/agc016_f.cpp
+++ AtCoder/agc016_f.cpp
@@ -1,97 +1,69 @@
 /**
+ * @file agc016_f.cpp
  * @author Macesuted (i@macesuted.moe)
- * @copyright Copyright (c) 2021
+ * @date 2022-07-15
+ *
+ * @copyright Copyright (c) 2022
+ *
  */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-namespace io {
-#define SIZE (1 << 20)
-char ibuf[SIZE], *iS, *iT, obuf[SIZE], *oS = obuf, *oT = oS + SIZE - 1, c, qu[55];
-int f, qr;
-inline void flush(void) { return fwrite(obuf, 1, oS - obuf, stdout), oS = obuf, void(); }
-inline char getch(void) { return (iS == iT ? (iT = (iS = ibuf) + fread(ibuf, 1, SIZE, stdin), (iS == iT ? EOF : *iS++)) : *iS++); }
-inline void putch(char x) {
-    *oS++ = x;
-    if (oS == oT) flush();
-    return;
-}
-string getstr(void) {
-    queue<char> que;
-    char c = getch();
-    while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF) c = getch();
-    while (!(c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == EOF)) que.push(c), c = getch();
-    string s;
-    s.resize(que.size());
-    for (register int i = 0; i < (int)s.size(); i++) s[i] = que.front(), que.pop();
-    return s;
-}
-void putstr(string str, int begin = 0, int end = -1) {
-    if (end == -1) end = str.size();
-    for (register int i = begin; i < end; i++) putch(str[i]);
-    return;
-}
-template <typename T>
-inline T read() {
-    register T x = 0;
-    for (f = 1, c = getch(); c < '0' || c > '9'; c = getch())
-        if (c == '-') f = -1;
-    for (x = 0; c <= '9' && c >= '0'; c = getch()) x = x * 10 + (c & 15);
-    return x * f;
-}
-template <typename T>
-inline void write(const T& t) {
-    register T x = t;
-    if (!x) putch('0');
-    if (x < 0) putch('-'), x = -x;
-    while (x) qu[++qr] = x % 10 + '0', x /= 10;
-    while (qr) putch(qu[qr--]);
-    return;
-}
-struct Flusher_ {
-    ~Flusher_() { flush(); }
-} io_flusher_;
-}  // namespace io
-using io::getch;
-using io::getstr;
-using io::putch;
-using io::putstr;
-using io::read;
-using io::write;
+#ifndef LOCAL
+#define endl '\n'
+#endif
 
-#define maxn 16
-#define maxm 400
+bool mem1;
+
+#define maxn 15
 #define mod 1000000007
 
-int graph[maxn][maxn], cnt[maxn][1 << maxn];
-long long f[1 << maxn], pow2[maxm];
+int con[maxn][1 << maxn];
+int64_t f[1 << maxn], pow2[1000];
 
-inline int lowbit(int x) { return x & -x; }
+void solve(void) {
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0, x, y; i < m; i++) cin >> x >> y, con[x - 1][1 << (y - 1)]++;
+    for (int i = 0; i < n; i++)
+        for (int j = 1; j < (1 << n); j++)
+            if (__builtin_popcount(j) > 1)
+                for (int k = 0; k < n; k++)
+                    if ((j >> k & 1)) con[i][j] += con[i][1 << k];
+    f[0] = 1;
+    for (int S = 1; S < (1 << n); S++) {
+        if ((S & 1) ^ (S >> 1 & 1)) continue;
+        for (int T = S; T; T = (T - 1) & S) {
+            if ((T & 1) ^ (T >> 1 & 1)) continue;
+            int64_t g = 1;
+            for (int i = 0; i < n; i++)
+                if ((S ^ T) >> i & 1) g = g * (pow2[con[i][T]] + mod - 1) % mod;
+            for (int i = 0; i < n; i++)
+                if (T >> i & 1) g = g * pow2[con[i][S ^ T]] % mod;
+            f[S] = (f[S] + f[S ^ T] * g) % mod;
+        }
+    }
+    cout << (pow2[m] + mod - f[(1 << n) - 1]) % mod << endl;
+    return;
+}
+
+bool mem2;
 
 int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
     pow2[0] = 1;
-    for (register int i = 1; i < maxm; i++) pow2[i] = pow2[i - 1] * 2 % mod;
-    int n = read<int>(), m = read<int>();
-    for (register int i = 1; i <= m; i++) {
-        int x = read<int>() - 1, y = read<int>() - 1;
-        graph[x][y]++;
-    }
-    for (register int i = 0; i < n; i++)
-        for (register int j = 1; j < (1 << n); j++)
-            cnt[i][j] = cnt[i][j ^ lowbit(j)] + graph[i][(int)log2(lowbit(j))];
-    f[0] = 1;
-    for (register int i = 1; i < (1 << n); i++)
-        for (register int j = i; j; j = (j - 1) & i) {
-            if ((j & 1) ^ (j >> 1 & 1)) continue;
-            long long ans = 1;
-            for (register int t = 0; t < n; t++)
-                if (j >> t & 1)
-                    ans = ans * pow2[cnt[t][i ^ j]] % mod;
-                else if (i >> t & 1)
-                    ans = ans * (pow2[cnt[t][j]] - 1) % mod;
-            f[i] = (f[i] + f[i ^ j] * ans) % mod;
-        }
-    write((pow2[m] + mod - f[(1 << n) - 1]) % mod), putch('\n');
+    for (int i = 1; i < 1000; i++) pow2[i] = pow2[i - 1] * 2 % mod;
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
     return 0;
 }
