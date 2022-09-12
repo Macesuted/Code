1f94cc87334bb218fc80021bf110fc6d1404e584
Macesuted <i@macesuted.moe>
2022-09-12 09:49:56 +0800

    AtCoder: arc148_e ≥ K

---
+++ AtCoder/arc148_e.cpp
@@ -0,0 +1,72 @@
+/**
+ * @file arc148_e.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-09-12
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+bool mem1;
+
+#define maxn 200005
+#define mod 998244353
+
+int64_t Pow(int64_t a, int64_t x) {
+    int64_t ans = 1;
+    while (x) {
+        if (x & 1) ans = ans * a % mod;
+        a = a * a % mod, x >>= 1;
+    }
+    return ans;
+}
+int64_t Inv(int64_t a) { return Pow(a, mod - 2); }
+
+int a[maxn];
+int64_t fac[maxn], ifac[maxn];
+map<int, int> rec;
+
+void solve(void) {
+    int n, k;
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) cin >> a[i], rec[a[i]]++;
+    sort(a + 1, a + n + 1);
+    int64_t ans = 1, cnt = 1;
+    for (int i = 1, j = n; i <= j;)
+        if (a[i] + a[j] < k)
+            ans = ans * cnt-- % mod, i++;
+        else
+            ans = ans * cnt++ % mod, j--;
+    for (auto i : rec) ans = ans * ifac[i.second] % mod;
+    cout << ans << endl;
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    fac[0] = ifac[0] = 1;
+    for (int i = 1; i < maxn; i++) fac[i] = fac[i - 1] * i % mod;
+    ifac[maxn - 1] = Inv(fac[maxn - 1]);
+    for (int i = maxn - 2; i; i--) ifac[i] = ifac[i + 1] * (i + 1) % mod;
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
