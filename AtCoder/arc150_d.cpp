1bf458c3777bb9ee98c693505d7747990081cff8
Macesuted <i@macesuted.moe>
2022-11-05 21:05:53 +0800

    AtCoder: arc150_d Removing Gacha

---
+++ AtCoder/arc150_d.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file arc150_d.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-11-05
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+bool mem1;
+
+#define maxn 200005
+#define mod 998244353
+
+int64_t Pow(int64_t a, int64_t x) {
+    int64_t ans = 1;
+    while (x) {
+        if (x & 1) ans = ans * a % mod;
+        a = a * a % mod, x >>= 1;
+    }
+    return ans;
+}
+int64_t Inv(int64_t a) { return Pow(a, mod - 2); }
+
+int dep[maxn];
+int64_t fac[maxn], ifac[maxn], inv[maxn], pre[maxn];
+
+void solve(void) {
+    int n, ans = 1;
+    cin >> n, dep[1] = 1;
+    for (int i = 2, x; i <= n; i++) cin >> x, ans = (ans + pre[dep[i] = dep[x] + 1]) % mod;
+    cout << ans << endl;
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    fac[0] = ifac[0] = 1;
+    for (int i = 1; i < maxn; i++) fac[i] = fac[i - 1] * i % mod;
+    ifac[maxn - 1] = Inv(fac[maxn - 1]);
+    for (int i = maxn - 2; i; i--) ifac[i] = ifac[i + 1] * (i + 1) % mod;
+    for (int i = 1; i < maxn; i++) pre[i] = pre[i - 1] + (inv[i] = fac[i - 1] * ifac[i] % mod);
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
