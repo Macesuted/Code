e288f2c3b08632259483b6106dafeda360d6d6bc
Macesuted <i@macesuted.moe>
2022-11-23 08:11:44 +0800

    Codeforces: 1748A The Ultimate Square

---
+++ Codeforces/1748A.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file 1748A.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2022-11-12
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+bool mem1;
+
+void solve(void) {
+    int n;
+    cin >> n;
+    cout << n / 2 + (n % 2 > 0) << endl;
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    cin >> _;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
