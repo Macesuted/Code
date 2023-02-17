ba6722845ac13f69538e50950634db3e1a5d1c46
Macesuted <i@macesuted.moe>
2023-02-17 10:17:10 +0800

    Nowcoder: 248247 惊鸿

---
+++ Nowcoder/248247.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file 248247.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2023-02-16
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+bool mem1;
+
+void solve(void) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d, a |= b | c | d;
+    cout << 4 * a << endl;
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    cin >> _;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
