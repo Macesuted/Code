6cdf76bd164492b37b97f12608b8eb324a01c4eb
Macesuted <i@macesuted.moe>
2024-01-07 10:28:46 +0800

    LibreOJ: 4002  「NOIP2023」三值逻辑

---
+++ LibreOJ/4002.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file 4002.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2023-12-24
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+bool mem1;
+
+void solve(void) { return; }
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
