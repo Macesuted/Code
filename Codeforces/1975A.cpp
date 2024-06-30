1f9aff0e33f170ac1cfeea98ebc2ae6595e79ebf
Macesuted <i@macesuted.moe>
2024-06-30 16:12:43 +0800

    Codeforces: 1975A Bazoka and Mocha's Array

---
+++ Codeforces/1975A.cpp
@@ -0,0 +1,54 @@
+/**
+ * @file 1975A.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2024-06-30
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+bool mem1;
+
+#define maxn 55
+
+int a[maxn];
+
+void solve(void) {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    for (int i = 1; i <= n; i++) {
+        bool chk = true;
+        for (int j = 1; j < n; j++) chk &= (a[j] <= a[j + 1]);
+        if (chk) return cout << "Yes" << endl, void();
+        for (int i = 1; i <= n; i++) a[i - 1] = a[i];
+        a[n] = a[0];
+    }
+    cout << "No" << endl;
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    cin >> _;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
