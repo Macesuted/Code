63e772681dfd5ebf02d7acdb175f7e7a5c312f68
Macesuted <i@macesuted.moe>
2026-04-29 12:08:17 +0000

    AtCoder: 455

---
+++ AtCoder/abc455_a.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file abc455_a.cpp
+ * @author Macesuted (i@macesuted.moe)
+ * @date 2026-04-25
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef LOCAL
+#define endl '\n'
+#endif
+
+bool mem1;
+
+void solve(void) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << (a != b && b == c ? "Yes" : "No") << endl;
+    return;
+}
+
+bool mem2;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr);
+#ifdef LOCAL
+    cerr << "Memory Cost: " << abs(&mem1 - &mem2) / 1024. / 1024. << "MB" << endl;
+#endif
+
+    int _ = 1;
+    while (_--) solve();
+
+#ifdef LOCAL
+    cerr << "Time Cost: " << clock() * 1000. / CLOCKS_PER_SEC << "MS" << endl;
+#endif
+    return 0;
+}
